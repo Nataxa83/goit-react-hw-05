204eedde318ee50fdeff1ba867ff0d62fd9a865b
Nataliia Hordiienko <Natalihordienko@gmail.com>
2024-11-02 20:47:54 +0100

    detalPage css

--- src/pages/MovieDatailsPage.jsx
+++ src/pages/MovieDatailsPage.jsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 import { getMovieDetails } from "../servises/api";
+
+import css from "./MovieDatailsPage.module.css"
 
 import MovieItem from "../components/MovieItem/MovieItem";
 import Loader from "../components/Loader/Loader";
@@ -13,6 +15,7 @@ const MovieDatailsPage = () => {
   const [loading, setLoading] = useState(false);
 
   const { movieId } = useParams("movieId");
+  const backLinkRef = useRef(location.state?.from ?? "/movies");
 
 
   useEffect(() => {
@@ -42,28 +45,40 @@ console.log(movieItem)
       {loading && <Loader />}
       {error && <p>{error}</p>}
       {movieItem ? (
-        <div>
+        <div className={css.container}>
+          <Link className={css.link} to={backLinkRef.current}>
+        Go back
+      </Link>
+          <div className={css.movieInfo}> 
+
+          <div className={css.movieItem}>
           <MovieItem
             id={movieItem.id}
             poster_path={movieItem.poster_path}
           />
-          <h1>{movieItem.title}</h1>
-          <p>User score: {Math.round(movieItem.vote_average*10)/10}</p>
-          <h2>Overview</h2>
+          </div>
+          <div>
+
+          <h1 className={css.title}>{movieItem.title}</h1>
+          <h3>Overview</h3>
           <p>{movieItem.overview}</p>
-          <h2>Genres</h2>
+          <h3>Genres</h3>
           {movieItem.genres && movieItem.genres.map((genre) => genre.name).join(', ')}
             
           
-          <h2>Prodaction countries</h2>
+          <h3>Prodaction countries</h3>
+          {movieItem.production_countries && movieItem.production_countries.map((country) => country.name).join(', ')}
           
 
-          <h3>Release date: {movieItem.release_date}</h3>
+          <p> <span className={css.info}>Release date:</span> {movieItem.release_date}</p>
+          <p> <span className={css.info}> Rating:</span> {Math.round(movieItem.vote_average*10)/10}</p>
+          </div>
+          </div>
         
         
-          <h2>Additional information</h2>
-              <Link to="cast">Cast</Link>
-              <Link to="reviews">Reviews</Link>
+          <h3> <span className={css.dopInfo}> Additional information </span></h3>
+              <Link className={css.link} to="cast">Cast</Link>
+              <Link className={css.link} to="reviews">Reviews</Link>
         
         
         </div>
